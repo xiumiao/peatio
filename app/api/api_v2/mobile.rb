02f137c28043e0e7c8f04033b391e5df0f081a5c
Jan Xie <jan.h.xie@gmail.com>
2014-11-04 10:58:38 +0800

    mobile api scaffold

---
+++ app/api/api_v2/mobile.rb
@@ -0,0 +1,22 @@
+module APIv2
+  class Mobile < Grape::API
+
+    desc 'Request mobile verification code.'
+    params do
+      requires :uid_type, type: Symbol, desc: "UID type", values: [:email, :phone_number]
+      requires :uid_value, type: String, desc: "UID value"
+    end
+    post "/mobile/verification_codes" do
+    end
+
+    desc 'Request user access token using verification code.'
+    params do
+      requires :uid_type, type: Symbol, desc: "UID type", values: [:email, :phone_number]
+      requires :uid_value, type: String, desc: "UID value"
+      requires :code, type: String, desc: "Verification code"
+    end
+    post "/mobile/access_tokens" do
+    end
+
+  end
+end
--- app/api/api_v2/mount.rb
+++ app/api/api_v2/mount.rb
@@ -31,6 +31,7 @@ module APIv2
     mount OrderBooks
     mount Trades
     mount K
+    mount Mobile
 
     base_path = Rails.env.production? ? "#{ENV['URL_SCHEMA']}://#{ENV['URL_HOST']}" : nil
     add_swagger_documentation base_path: base_path,
