12fcd3b67638641a6f79e5f8b5bcbfdba39f4260
Bin He <beenhero@gmail.com>
2015-04-05 19:49:28 +0800

    show latest announcement on settings page. no matter public or private, just the latest one.

--- app/controllers/private/settings_controller.rb
+++ app/controllers/private/settings_controller.rb
@@ -1,6 +1,7 @@
 module Private
   class SettingsController < BaseController
     def index
+      @announcement = Announcement.latest
       unless current_user.activated?
         flash.now[:info] = t('.activated')
       end
