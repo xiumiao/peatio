70a6e57c633bfda49ed40ca896a11d14287b08f9
lixiumiao <lixiumiao@gmail.com>
2015-11-22 23:59:41 +0800

    nothing

--- auto_create_order.rb
+++ auto_create_order.rb
@@ -60,32 +60,28 @@ loop do
 	# 上升或下降趋势控制：随机上升或是下降：然后
 	# 应该获取市场深度，如需要价格上升到某个价格，直接购买对应深度时的价格
 	depth = client1.get '/api/v2/depth.json', market: 'puercny'
-	ticker = client1.get '/api/v2/tickers/puercny.json', market: 'puercny'
 	# 未取出上一次的价格时
 	next unless last == 0
+	amount_a = [0,0]
+	price_a= [0,0]
 	up_or_down = rand(2)  unless up_or_down
-  if up_or_down == 0 
-		if 10.minutes.ago > big_deal_time
-			buy_amount = rand(4.5..6.2)  
-			big_deal_time = Time.now
-		else
-			buy_amount = rand(0.09..0.12)
-		end
-		sell_amount = rand(0.08..0.11)
-		上升时，上升幅度与上升次数成反比
-	  buy_price = rand(last..last*(1.1%(1+up_times).round(2))
-		sell_price = rand(buy_price*0.9..buy_price*1.05).round(2)
-		up_times =0 if up_times > 10
-		down_times = 0
+	if 5.minutes.ago > big_deal_time
+		buy_amount = rand(4.5..6.2)
+		big_deal_time = Time.now
 	else
-
+		buy_amount = rand(0.09..0.12)
 	end
-	
+	sell_amount = rand(0.08..0.11)
+	上升时，上升幅度与上升次数成反比
+  buy_price = rand(last..last*(1.1%(1+up_times).round(2))
+	sell_price = rand(buy_price*0.9..buy_price*1.05).round(2)
+  amount_a,price_a =  up_or_down == 0 ? [[buy_amount,sell_amount],[buy_price,sell_price]] : [[sell_amount,buy_amount],[sell_price,buy_price]]
 	# 上一次交易价格
+	ticker = client1.get '/api/v2/tickers/puercny.json', market: 'puercny'
 	last = ticker['ticker']['last'].to_f
 
-	client1.post '/api/v2/orders', market: 'puercny', side: 'sell', volume: rand(0.1..0.15).round(2), price: rand(last*0.7..last*1.3).round(2)
-	client2.post '/api/v2/orders', market: 'puercny', side: 'buy', volume: rand(0.1..0.15).round(2), price: rand(last*0.7..last*1.3).round(2)
+	client1.post '/api/v2/orders', market: 'puercny', side: 'sell', volume: amount_a[0], price: price_a[0]
+	client2.post '/api/v2/orders', market: 'puercny', side: 'buy', volume: amount_a[1], price:  price_a[1]
 end
 # POST to create multiple orders at once
 #client1.post '/api/v2/orders/multi', market: 'puercny', orders: [{side: 'buy', volume: '0.15', price: '2955.0'}, {side: 'sell', volume: '0.16', price: '2956'}]
