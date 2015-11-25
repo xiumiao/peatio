e5c1f0949b5eda20870f5b7b25c46615d7ebdba3
lixiumiao <lixiumiao@gmail.com>
2015-11-26 02:49:17 +0800

    application.yml的配置需要更改,ws和api传递的数据应该不一样,造成这样的问题

--- app/assets/javascripts/lib/pusher_connection.js.coffee
+++ app/assets/javascripts/lib/pusher_connection.js.coffee
@@ -3,5 +3,9 @@ pusher = new Pusher gon.pusher.key,
   wsHost: gon.pusher.wsHost
   wsPort: gon.pusher.wsPort
   wssPort: gon.pusher.wssPort
+  disableFlash: !0
+  disableStats: !0
+  enabledTransports: ['ws']
+  disabledTransports: ["flash","sockjs"]
 
 window.pusher = pusher
--- auto_create_order.rb
+++ auto_create_order.rb
@@ -35,13 +35,13 @@ end
 
 
 
-client_public = PeatioAPI::Client.new endpoint: 'http://127.0.0.1:3000'
+client_public = PeatioAPI::Client.new endpoint: 'http://127.0.0.1'
 
 # GET public api /api/v2/markets
 # client_public.get_public '/api/v2/markets'
-client1 = 	PeatioAPI::Client.new access_key: '3pBSw9yXOocDEaSsr5q78oK7k7dPrQxqrFszfbpq', secret_key: '13KUuefVsMxs6riLZZiFansrWjw61jOxIP4xJKE3', endpoint: 'http://127.0.0.1:3000', timeout: 60
+client1 = 	PeatioAPI::Client.new access_key: 'hInVroSJXK0shc9t96H7Ub16YK0G3ccjOBlcgXAG', secret_key: 'xIhaOxzLUMyD57b1xqpbZmXFODsU5gc9CRyrHmxa', endpoint: 'http://127.0.0.1:3000', timeout: 60
 
-client2 = PeatioAPI::StreamingClient.new access_key: 'E2Oz8OXIyMvWZqRPdeYsgPpnbPsaYKGfhAmSgojS', secret_key: 'sAiIrAxoWej3Us1LJFHShwrkaLTzKskFB6j0M31A', endpoint: 'http://127.0.0.1:3000'
+client2 = PeatioAPI::StreamingClient.new access_key: 'kwW8dycJPwEUDwafaIBcx3f3tNnMN8YnKM9YkAWW', secret_key: 't6ESANqvALxVxLvO6vjoxCeYGDSNeiKXPsOlkwEc', endpoint: 'http://127.0.0.1:3000'
 
 # 最近一次交易价格
 last = 200
@@ -59,6 +59,7 @@ loop do
 	# 上一次交易价格
 	ticker = client1.get '/api/v2/tickers/puercny.json', market: 'puercny'
 	last = ticker['ticker']['last'].to_f
+	#binding.pry
 	# 大部分交易在0.1千克，价格200左右，
 	# 大单控制在1千克左右，
 	# 上升或下降趋势控制：随机上升或是下降：然后
@@ -67,19 +68,19 @@ loop do
 	# 未取出上一次的价格时
 	amount_a = [0,0]
 	price_a= [0,0]
-	up_or_down = rand(2)
+	up_or_down = rand(3)
 	if 5.minutes.ago > big_deal_time
 		buy_amount = rand(4.5..6.2)
 		big_deal_time = Time.now
 	else
 		buy_amount = rand(0.09..0.12)
 	end
-	sell_amount = rand(0.08..0.11)
+	sell_amount = rand(0.08..0.12)
 	#上升时，上升幅度与上升次数成反比
-  buy_price = rand(last..last*(1.1/(1+up_times).round(2)))
-	#binding.pry
-	sell_price = rand(buy_price*0.9..buy_price*1.05).round(2)
-  amount_a,price_a =  up_or_down == 0 ? [[buy_amount,sell_amount],[buy_price,sell_price]] : [[sell_amount,buy_amount],[sell_price,buy_price]]
+	buy_price = rand(last..last*(1.1/(1+up_times).round(2)))
+	#requirebinding.pry
+	sell_price = rand(buy_price*0.8..buy_price*1.15).round(2)
+	amount_a,price_a =  up_or_down == 0 ? [[buy_amount,sell_amount.to_f],[buy_price,sell_price.to_f]] : [[sell_amount,buy_amount.to_f],[sell_price,buy_price.to_f]]
 
 	client1.post '/api/v2/orders', market: 'puercny', side: 'sell', volume: amount_a[0], price: price_a[0]
 	client2.post '/api/v2/orders', market: 'puercny', side: 'buy', volume: amount_a[1], price:  price_a[1]
